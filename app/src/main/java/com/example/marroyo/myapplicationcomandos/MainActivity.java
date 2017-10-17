cbcd7190c3a80796c2c17fddeff8c57d61625e22
Manuel <manuelarroyo1998@gmail.com>
2017-10-17 09:57:10 +0200

    Añadido el boton

--- app/src/main/java/com/example/marroyo/myapplicationcomandos/MainActivity.java
+++ app/src/main/java/com/example/marroyo/myapplicationcomandos/MainActivity.java
@@ -2,12 +2,17 @@ package com.example.marroyo.myapplicationcomandos;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+
+    private Button btn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        btn = (Button)findViewById(R.id.button);
     }
 }
